765fff6c108f4def3e5d94edcf0c4a1086ec4da1
rosendo <rosendo2018@163.com>
2022-05-23 16:32:43 +0800

    Integrate MUI

--- src/components/Demo.tsx
+++ src/components/Demo.tsx
@@ -1,10 +1,11 @@
+import { Alert, Button } from '@mui/material';
+
 export default function Demo() {
   return (
     <>
-
-
-
+      <Button>MUI button</Button>
       <h3 className="bg-main-btn">Demo</h3>
+      <Alert severity="success">successfully done</Alert>
     </>
   );
 }
